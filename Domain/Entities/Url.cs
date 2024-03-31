ff6ee0a7f35c02db5ae6b9e392f50bcc71b96521
ArthurCovelo <arthurcovelo@live.com>
2024-03-31 20:41:11 -0300

    Criando Entidades e Interfaces

---
+++ Domain/Entities/Url.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    [Table("urls")]
+    public class Url
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("link")]
+        public string Link { get; set; }
+    }
+}
---
+++ Domain/Interfaces/IUrlRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IUrlRepository
+    {
+        Task<Url> AddAsync(Url url);
+        Task DeleteAsync(int id);
+        Task<IEnumerable<Url>> ListUrlsAsync();
+        Task<Url> GetByIdAsync(int id);
+        Task UpdateAsync(Url url);
+    }
+}
