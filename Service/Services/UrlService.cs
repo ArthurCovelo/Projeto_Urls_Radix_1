17e45eb46ebddaccc257826f1071b310bf8366fb
ArthurCovelo <arthurcovelo@live.com>
2024-03-31 21:11:29 -0300

    Criando servico do url e validacoes.

---
+++ Service/Services/UrlService.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Service
+{
+    public class UrlService : IUrlService
+    {
+        private readonly IUrlRepository _urlRepository;
+
+        public UrlService(IUrlRepository urlRepository)
+        {
+            _urlRepository = urlRepository;
+        }
+
+        public async Task<Url> AddAsync(Url url)
+        {
+            return await _urlRepository.AddAsync(url);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            await _urlRepository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<Url>> ListUrlsAsync()
+        {
+            return await _urlRepository.ListUrlsAsync();
+        }
+
+        public async Task<Url> GetByIdAsync(int id)
+        {
+            return await _urlRepository.GetByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(Url url)
+        {
+            await _urlRepository.UpdateAsync(url);
+        }
+    }
+}
---
+++ Service/Validation/Validacoes.cs
@@ -0,0 +1,25 @@
+﻿namespace Service.Validation
+{
+    public class Validacoes
+    {
+        public bool IsUrlValid(string url, out IDictionary<string, string> validationErrors)
+        {
+            validationErrors = new Dictionary<string, string>();
+
+            if (string.IsNullOrEmpty(url))
+            {
+                validationErrors.Add("Link", "A URL não pode ser vazia.");
+                return false;
+            }
+
+            if (!Uri.TryCreate(url, UriKind.Absolute, out Uri uriResult) ||
+                !(uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps))
+            {
+                validationErrors.Add("Link", "A URL é inválida.");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
