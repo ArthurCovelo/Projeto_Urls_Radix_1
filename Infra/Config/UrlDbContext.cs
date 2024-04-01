edb92919f06f47cff60218fe31adaac3a1462cd5
ArthurCovelo <arthurcovelo@live.com>
2024-03-31 21:03:49 -0300

    Criando Context e Repositorio, fazendo referencia ao Domain

---
+++ Infra/Config/UrlDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Config
+{
+    public class UrlDbContext : DbContext
+    {
+        public UrlDbContext(DbContextOptions<UrlDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Url> Urls { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            try
+            {
+                base.OnModelCreating(modelBuilder);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao definir o modelo do banco de dados.", ex);
+            }
+        }
+    }
+}
+
---
+++ Infra/Repositorio/UrlRepository.cs
@@ -0,0 +1,65 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infra.Config;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repositorio
+{
+    public class UrlRepository : IUrlRepository
+    {
+        private readonly UrlDbContext _dbContext;
+
+        public UrlRepository(UrlDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Url> AddAsync(Url url)
+        {
+            try
+            {
+                _dbContext.Urls.Add(url);
+                await _dbContext.SaveChangesAsync();
+                return url;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao adicionar a URL.", ex);
+            }
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var url = await _dbContext.Urls.FindAsync(id);
+            if (url != null)
+            {
+                _dbContext.Urls.Remove(url);
+                await _dbContext.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Url>> ListUrlsAsync()
+        {
+            return await _dbContext.Urls.ToListAsync();
+        }
+
+        public async Task<Url> GetByIdAsync(int id)
+        {
+            return await _dbContext.Urls.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(Url url)
+        {
+            try
+            {
+                _dbContext.Entry(url).State = EntityState.Modified;
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Ocorreu um erro ao atualizar a URL.", ex);
+            }
+        }
+
+    }
+}
