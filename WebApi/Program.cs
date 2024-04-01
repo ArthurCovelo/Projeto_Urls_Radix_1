5ec3f31d64c5ab8e1269f25059716fe4638ad2cb
ArthurCovelo <arthurcovelo@live.com>
2024-03-31 21:26:37 -0300

    Configurando a string de conexoes e program, adicionando a injecao de dependencia no program.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,11 +1,24 @@
+using Domain.Interfaces;
+using Infra.Config;
+using Infra.Repositorio;
+using Microsoft.EntityFrameworkCore;
+using Service;
+using Service.Validation;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var configuration = builder.Configuration;
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUrlRepository, UrlRepository>();
+builder.Services.AddScoped<UrlService>();
+builder.Services.AddScoped<Validacoes>();
+builder.Services.AddDbContext<UrlDbContext>(options =>
+options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
